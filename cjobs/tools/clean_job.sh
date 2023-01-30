91f0f3d68879e8fbbe6de8063a2849ba461168c5
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-30 12:14:11 -0300

    feat: add clean job function

---
+++ cjobs/tools/clean_job.sh
@@ -0,0 +1,15 @@
+function clean_job() {
+  echo "Received SIG"
+  if [[ -d "$job_scratch_dir" ]]; then
+    echo "Copying outputs to local_dir"
+    cp -vr "$job_scratch_dir"/* "$job_local_dir"/
+    echo "Removing scratch dir"
+    rm -vrf "$job_scratch_dir"
+    echo "Job done"
+    echo ""
+  else
+    echo "scratch dir does not exist"
+    echo ""
+  fi
+  exit
+}
