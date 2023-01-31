ebdc2db206509323e1833effc36b44a31e02f0f7
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-31 11:26:40 -0300

    feat: add pbs_pro header

--- cjobs/tools/scheduler_tools.py
+++ cjobs/tools/scheduler_tools.py
@@ -10,6 +10,12 @@ def build_scheduler_header(scheduler, job_name, n_cores, memory, job_time, n_job
         if n_jobs > 1:
             scheduler_header.append(f"#SBATCH --array=1-{n_jobs}")
         scheduler_header.append(f"#SBATCH --signal=B:TERM@60")
+    elif scheduler == 'pbs-pro':
+        scheduler_header.append(f'#PBS -N {job_name}')
+        scheduler_header.append(f'#PBS -o {job_name}.stdout')
+        scheduler_header.append(f'#PBS -e {job_name}.stderr')
+        scheduler_header.append(f'#PBS -l select=1:ncpus={n_cores}:mem={n_cores*memory}mb')
+        scheduler_header.append(f'#PBS -l walltime={job_time}')
     else:
         return None
     return scheduler_header
