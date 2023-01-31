a021c569a4a4333285ed7785c04a2e6a4d0c19df
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-31 09:36:37 -0300

    fix: correct typo in get_containers

--- cjobs/tools/get_containers.sh
+++ cjobs/tools/get_containers.sh
@@ -3,10 +3,10 @@ function get_containers() {
   requested_container="$2"
   if [[ ! -d $containers_dir ]]; then
     mkdir $containers_dir
-    rclone cp "$requested_container" "$container_dir"
+    rclone copy "$requested_container" "$containers_dir"
   else
     if [[ ! -e "$containers_dir"/"$requested_container" ]]; then
-      rclone copy "$requested_container" "$container_dir"
+      rclone copy "$requested_container" "$containers_dir"
     fi
   fi    
 }
