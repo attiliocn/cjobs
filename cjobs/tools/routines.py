e7267e3315e813e65735dcecd7c7602602376040
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-31 11:53:59 -0300

    feat: add xtb routine

--- cjobs/tools/routines.py
+++ cjobs/tools/routines.py
@@ -11,3 +11,14 @@ def build_gaussian16_routine(g16_scrdir, n_cores, job_input, container):
     g16_routine.append(f'')
     g16_routine.append(f'singularity run {container} g16 < "${job_input}" > "$job_output"')
     return g16_routine
+
+def build_xtb_routine(n_cores, job_input, flags, container):
+    xtb_routine = []
+    xtb_routine.append(f'')
+    xtb_routine.append(f'export MKL_NUM_THREADS={n_cores}')
+    xtb_routine.append(f'export OMP_NUM_THREADS={n_cores}')
+    xtb_routine.append(f'export OMP_STACKSIZE=4G')
+    xtb_routine.append(f'ulimit -s unlimited')
+    xtb_routine.append(f'')
+    xtb_routine.append(f'singularity run {container} xtb "${job_input}" {" ".join(flags)}  &> xtb.output')
+    return xtb_routine
