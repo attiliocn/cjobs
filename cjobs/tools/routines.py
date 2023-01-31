f39613f45066b8a5c8bfe609cb82caab60b288c6
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-31 11:06:24 -0300

    fix: implement gauss_srcdir

--- cjobs/tools/routines.py
+++ cjobs/tools/routines.py
@@ -1,10 +1,8 @@
 def build_gaussian16_routine(g16_scrdir, n_cores, job_input, container):
-    job_output = f'$(cut -f 1 -d \'.\' "${job_input}").log'
-
     g16_routine = []
-    g16_routine.append(f'export $GAUSS_SCRDIR={g16_scrdir}')
+    g16_routine.append(f'export $GAUSS_SCRDIR="{g16_scrdir}"')
     g16_routine.append(f'mkdir $GAUSS_SCRDIR')
-    g16_routine.append(f'job_output=$(cut -f 1 -d \'.\' "${job_input}").log')
+    g16_routine.append(f'job_output=""$(echo "${job_input}" | rev | cut -f 2- -d \'.\' | rev)".log"')
     g16_routine.append(f'')
     g16_routine.append(f'export MKL_NUM_THREADS={n_cores}')
     g16_routine.append(f'export OMP_NUM_THREADS={n_cores}')
