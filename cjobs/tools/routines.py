d7d11d8d012d3072c36135eca3aeee4aaf3befab
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-31 10:21:12 -0300

    fix: typo in g16 routine

--- cjobs/tools/routines.py
+++ cjobs/tools/routines.py
@@ -8,7 +8,7 @@ def build_gaussian16_routine(g16_scrdir, n_cores, job_input, container):
     g16_routine.append(f'')
     g16_routine.append(f'export MKL_NUM_THREADS={n_cores}')
     g16_routine.append(f'export OMP_NUM_THREADS={n_cores}')
-    g16_routine.append(f'OMP_STACKSIZE=4G')
+    g16_routine.append(f'export OMP_STACKSIZE=4G')
     g16_routine.append(f'ulimit -s unlimited')
     g16_routine.append(f'')
     g16_routine.append(f'singularity run {container} g16 < "${job_input}" > "$job_output"')
