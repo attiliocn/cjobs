db28f030735bfbe03f1e01daf5f17ead2fe5c0ca
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-31 18:36:29 -0300

    fix: add binds to g16 routine

--- cjobs/tools/routines.py
+++ cjobs/tools/routines.py
@@ -1,15 +1,23 @@
-def build_gaussian16_routine(g16_scrdir, n_cores, job_input, container):
+def build_gaussian16_routine(scrdir, n_cores, job_input, container):
     g16_routine = []
-    g16_routine.append(f'export $GAUSS_SCRDIR="{g16_scrdir}"')
+    g16_routine.append(f'export $GAUSS_SCRDIR="${scrdir}"/src')
     g16_routine.append(f'mkdir $GAUSS_SCRDIR')
-    g16_routine.append(f'job_output=""$(echo "${job_input}" | rev | cut -f 2- -d \'.\' | rev)".log"')
     g16_routine.append(f'')
     g16_routine.append(f'export MKL_NUM_THREADS={n_cores}')
     g16_routine.append(f'export OMP_NUM_THREADS={n_cores}')
     g16_routine.append(f'export OMP_STACKSIZE=4G')
     g16_routine.append(f'ulimit -s unlimited')
     g16_routine.append(f'')
-    g16_routine.append(f'singularity run {container} g16 < "${job_input}" > "$job_output"')
+    g16_routine.append(
+        f'''singularity run \\
+    --bind="${scrdir}":"${scrdir}" \\
+    {container} \\
+    "${scrdir}" \\
+    g16 "${job_input}" &
+'''
+)
+    g16_routine.append('wait')
+    g16_routine.append('exit')
     return g16_routine
 
 def build_xtb_routine(scrdir, n_cores, job_input, flags, container):
