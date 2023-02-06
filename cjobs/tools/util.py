5378df7609357054e81c0d911126f62987911d16
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-02-06 16:04:41 -0300

    fix: remove arg from get_hash

--- cjobs/tools/util.py
+++ cjobs/tools/util.py
@@ -5,6 +5,6 @@ def get_timestamp():
     current_time = datetime.now()
     return current_time.strftime('%Y-%m-%d_%H-%M-%S')
 
-def get_hash_from_timestamp(timestamp):
+def get_hash_from_timestamp():
     ts = get_timestamp()
     return hashlib.sha1(ts.encode('utf-8')).hexdigest()
