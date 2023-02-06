44263f48d326f96aace9764e629484e322698b1c
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-02-06 15:49:36 -0300

    fix: update rules for array jobs for slurm

--- cjobs/tools/scheduler_tools.py
+++ cjobs/tools/scheduler_tools.py
@@ -1,4 +1,4 @@
-def build_scheduler_header(scheduler, job_name, n_cores, memory, job_time, n_jobs=1):
+def build_scheduler_header(scheduler, job_name, n_cores, memory, job_time, n_jobs=1, job_array=False):
     scheduler_header = []
     if scheduler == 'slurm':
         scheduler_header.append(f"#SBATCH --job-name {job_name}")
@@ -7,7 +7,7 @@ def build_scheduler_header(scheduler, job_name, n_cores, memory, job_time, n_job
         scheduler_header.append(f"#SBATCH --ntasks={n_cores}")
         scheduler_header.append(f"#SBATCH --mem-per-cpu={memory}")
         scheduler_header.append(f"#SBATCH --time={job_time}")
-        if n_jobs > 1:
+        if n_jobs > 1 and job_array == True:
             scheduler_header.append(f"#SBATCH --array=1-{n_jobs}")
         scheduler_header.append(f"#SBATCH --signal=B:TERM@60")
     elif scheduler == 'pbs-pro':
