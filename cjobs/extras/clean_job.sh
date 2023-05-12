18a66bbb6ac5b0286b3e1c02c49501204e8ddc9c
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-05-12 16:20:04 -0300

    fix: refactor of clean_job function

--- cjobs/extras/clean_job.sh
+++ cjobs/extras/clean_job.sh
@@ -1,16 +1,18 @@
 function clean_job() {
-  echo "Received SIG"
-  if [[ -d "$job_scratch_dir" ]]; then
-    echo "Copying outputs to local_dir"
-    cp -vr "$job_scratch_dir" "$job_local_dir"/"$job_name"
-    echo "Removing scratch dir"
-    rm -vrf "$job_scratch_dir"
-    echo "Job done"
-    echo ""
-  else
-    echo "scratch dir does not exist"
-    echo ""
-  fi
-  exit
+    echo "Received signal."
+    if [[ -d "$job_scratch_dir" ]]; then
+        echo "Copying outputs to local directory."
+        cp -vr "$job_scratch_dir" "$job_local_dir"/"$job_name"
+        echo "Successfully copied outputs."
+        echo "Removing scratch directory."
+        rm -vrf "$job_scratch_dir"
+        echo "Scratch directory removed."
+        echo "Job completed."
+        echo ""
+    else
+        echo "Scratch directory does not exist."
+        echo ""
+    fi
+    exit
 }
 trap clean_job EXIT HUP INT TERM ERR
