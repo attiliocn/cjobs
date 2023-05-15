921285332c2d82ca9091847c447263d0f1595a27
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-05-15 12:44:16 -0300

    fix: use flock for lock aquisition

--- cjobs/extras/fetch_containers_from_drive.sh
+++ cjobs/extras/fetch_containers_from_drive.sh
@@ -9,42 +9,54 @@ function fetch_containers_from_drive() {
     max_wait_time=$((30 * 60))  # 30 minutes in seconds
     wait_time=0
 
-    # Check if the lock file exists
-    if [ -e "$lock_file" ]; then
-        echo "Lock file already exists. Another instance of the script may be running."
-        while [ -e "$lock_file" ]; do
-            if [ "$wait_time" -gt "$max_wait_time" ]; then
+    echo "Attempting to acquire lock for RClone usage."
+    # Attempt to acquire the lock
+    while true; do
+        exec 3>"$lock_file"
+        if flock -n 3; then
+        echo "Successfully acquired lock for for RClone usage."
+            break
+        fi
+        echo "Waiting for the lock to be released..."
+        sleep 5
+        wait_time=$((wait_time + 5))
+        if [ "$wait_time" -gt "$max_wait_time" ]; then
                 echo "Maximum wait time exceeded. Exiting..."
                 exit 1
             fi
-            echo "Waiting for the lock to be released..."
-            sleep 5
-            wait_time=$((wait_time + 5))
-        done
-    fi
+    done
     
     # Check if the synced file exists and is older than 1 week
     if [ -e "$synced_file" ]; then
         file_age=$(($(date +%s) - $(stat -c %Y "$synced_file")))
+
         if [ "$file_age" -lt 604800 ]; then
             echo "The last sync was done "$file_age" seconds ago. Actions will not be performed."
+            # Release the lock
+            exec 3>&-
+            rm $lock_file
+            echo "The lock has been released."
             return
         fi
+        
     fi
 
     if [[ ! -x  "$(command -v rclone)" ]]; then
         echo "The rclone command is unavailable. No synchronization will be performed."
+        # Release the lock
+        exec 3>&-
+        rm $lock_file
+        echo "The lock has been released."
         return
     fi
-
-    # Create the lock file
-    touch "$lock_file"
-    echo "Lock file is placed"
   
     rclone sync -v "$container_cloud_dir" "$container_local_dir"
     
-    rm "$lock_file"
-    echo "Lock file removed. Actions completed successfully."
+    # Release the lock
+    exec 3>&-
+    rm $lock_file
+    echo "The lock has been released."
+
     touch $synced_file
     echo "Updated last_sync.lock file"
     
