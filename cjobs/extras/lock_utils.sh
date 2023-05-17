780b19faf8f1a91a418a31cc1f93e9c1f7251e0e
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-05-16 21:40:35 -0300

    fix: refactor lock acquisition and release functions

--- cjobs/extras/lock_utils.sh
+++ cjobs/extras/lock_utils.sh
@@ -1,11 +1,15 @@
 acquire_lock() {
     local lock_name=$1
     local lock_location=$2
-    local lock_file="$lock_location"/"$lock_name"
-    
+    local lock_file="$lock_location/$lock_name"
+
     exec 3>"$lock_file"
-    flock -n 3
-    echo "Successfully acquired lock for $lock_name at $lock_location."
+    if flock -n -e 3; then
+        echo "Successfully acquired lock for $lock_name at $lock_location."
+        return true
+    else
+        return false
+    fi
 }
 
 release_lock() {
