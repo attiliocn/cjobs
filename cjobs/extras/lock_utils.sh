6144c99bb1e56e4663e4cfc1ffd2559ac6bc4778
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-05-16 21:14:18 -0300

    fix: isolate lock placement and release

--- cjobs/extras/lock_utils.sh
+++ cjobs/extras/lock_utils.sh
@@ -17,3 +17,20 @@ release_lock() {
     rm "$lock_file"
     echo "The lock for $lock_name at $lock_location has been released."
 }
+
+attempt_acquire_lock() {
+    local lock_name=$1
+    local lock_location=$2
+    local max_wait_time=$3
+    local wait_time=0
+    
+    while ! acquire_lock "$lock_name" "$lock_location"; do
+        echo "Waiting for the lock to be released..."
+        sleep 5
+        wait_time=$((wait_time + 5))
+        if [ "$wait_time" -gt "$max_wait_time" ]; then
+            echo "Maximum wait time exceeded. Exiting..."
+            exit 1
+        fi
+    done
+}
