8f7624476e1a11ee6e1e1061222dc9e505461cde
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-24 11:02:24 -0300

    create a JobDetails object

---
+++ cjobs/tools/jobdetails.py
@@ -0,0 +1,12 @@
+import datetime
+from tools.util import get_basename
+
+
+class JobDetails():
+    def __init__(self, filenames):
+        self.filenames = filenames
+        self.basenames = [get_basename(s) for s in self.filenames]
+        self.numJobs = len(self.filenames)
+
+    def write_joblist(self, filename):
+        pass
