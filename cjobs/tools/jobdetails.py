2f5fc42aecd417d8d38d07fcb220a470b17a5f29
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-24 18:44:43 -0300

    add bash functions related to jobname and basename

--- cjobs/tools/jobdetails.py
+++ cjobs/tools/jobdetails.py
@@ -4,6 +4,8 @@ class JobDetails():
     def __init__(self, filenames):
         self.filenames = filenames
         self.basenames = [pathlib.Path(s).stem for s in self.filenames]
+        self.bashJobname = ''
+        self.bashBasename = fr"$(echo $job | rev | cut -f 2- -d '.' | rev)"
         self.numJobs = len(self.filenames)
         self.sendAdditionalFiles = False
 
