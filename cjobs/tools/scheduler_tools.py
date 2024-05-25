d787d45246824ea9ab11e2e90c74993f95d7ff4c
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-25 10:28:13 -0300

    update scheduler header to OO mode

--- cjobs/tools/scheduler_tools.py
+++ cjobs/tools/scheduler_tools.py
@@ -1,23 +1,23 @@
-def build_scheduler_header(scheduler, job_name, n_cores, memory, job_time, n_jobs=1, job_array=False):
+def build_scheduler_header(job:object):
     scheduler_header = []
-    if scheduler == 'slurm':
-        scheduler_header.append(f"#SBATCH --job-name {job_name}")
+    if job.scheduler == 'slurm':
+        scheduler_header.append(f"#SBATCH --job-name {job.cjobsID}")
         scheduler_header.append(f"#SBATCH --output=job_%x_%J.stdout")
         scheduler_header.append(f"#SBATCH --nodes=1")
-        scheduler_header.append(f"#SBATCH --ntasks={n_cores}")
-        scheduler_header.append(f"#SBATCH --mem-per-cpu={memory}")
-        scheduler_header.append(f"#SBATCH --time={job_time}")
-        if n_jobs > 1 and job_array == True:
-            scheduler_header.append(f"#SBATCH --array=1-{n_jobs}")
+        scheduler_header.append(f"#SBATCH --ntasks={job.cpu}")
+        scheduler_header.append(f"#SBATCH --mem-per-cpu={job.ram}")
+        scheduler_header.append(f"#SBATCH --time={job.time}")
+        if job.isArray == True:
+            scheduler_header.append(f"#SBATCH --array=1-{job.numJobs}")
         scheduler_header.append(f"#SBATCH --signal=B:TERM@60")
-    elif scheduler == 'pbs-pro':
-        scheduler_header.append(f'#PBS -N {job_name}')
-        scheduler_header.append(f'#PBS -o {job_name}.stdout')
-        scheduler_header.append(f'#PBS -e {job_name}.stderr')
-        scheduler_header.append(f'#PBS -l select=1:ncpus={n_cores}:mem={n_cores*memory}mb')
-        scheduler_header.append(f'#PBS -l walltime={job_time}')
-        if n_jobs > 1 and job_array == True:
-            scheduler_header.append(f'#PBS -J 1-{n_jobs}:1')
+    elif job.scheduler == 'pbs-pro':
+        scheduler_header.append(f'#PBS -N {job.cjobsID}')
+        scheduler_header.append(f'#PBS -o {job.cjobsID}.stdout')
+        scheduler_header.append(f'#PBS -e {job.cjobsID}.stderr')
+        scheduler_header.append(f'#PBS -l select=1:ncpus={job.cpu}:mem={job.cpu*job.ram}mb')
+        scheduler_header.append(f'#PBS -l walltime={job.time}')
+        if job.isArray == True:
+            scheduler_header.append(f'#PBS -J 1-{job.numJobs}:1')
     else:
         return None
     return scheduler_header
