d92ac11ba4e2233adb5ed4354fcfd991d076333b
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-26 09:42:52 -0300

    remove blanks

--- cjobs/extras/fetch_containers_from_drive.sh
+++ cjobs/extras/fetch_containers_from_drive.sh
@@ -1,8 +1,8 @@
 function fetch_containers_from_drive() {
     container_cloud_dir="$1"
     container_local_dir="$2"
-    container_local_dir_parent=$(dirname "$container_local_dir") 
-    
+    container_local_dir_parent=$(dirname "$container_local_dir")
+
     synced_file="$container_local_dir_parent/last_sync.lock"
 
     # Check if the synced file exists and is older than 1 week
