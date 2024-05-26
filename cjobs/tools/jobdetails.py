441cffa160765c7d597a63f3b1d748e6b7baeeff
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-26 09:44:06 -0300

    add all properties to JobDetails init

--- cjobs/tools/jobdetails.py
+++ cjobs/tools/jobdetails.py
@@ -1,18 +1,45 @@
 import pathlib
 
 class JobDetails():
-    def __init__(self, filenames):
-        self.filenames = filenames
-        self.basenames = [pathlib.Path(s).stem for s in self.filenames]
-        self.bashJobname = ''
+    def __init__(self, jobFilenames):
+        self.jobFilenames = jobFilenames
+        self.jobBasenames = [pathlib.Path(s).stem for s in self.jobFilenames]
+        self.numJobs = len(self.jobFilenames)
+        self.jobNumber = 0
+        self.cjobsID = None
+        
+        self.scheduler = None
+        self.software = None
+        self.containerFile = None
+        self.singularityVersion = None
+        self.GID = None
+        
+        self.mode = 'single'
+        self.isArray = False
+        self.isMassive = False
+        
+        self.cpu = None
+        self.ram = None
+        self.time = None
+        
+        self.options = None
+        self.standalone = False
+        
+        self.localDir = None
+        self.scrDir = None
+        self.usrScrDir = None
+        self.exeDir = None
+        self.ctDirRemote = None
+        self.ctDirLocal = None
+        self.bashJobname = '$(get_csv_element cjobs_joblist.csv "$job_number" 2)'
         self.bashBasename = fr"$(echo $job | rev | cut -f 2- -d '.' | rev)"
-        self.numJobs = len(self.filenames)
+        
         self.sendAdditionalFiles = False
         self.additionalFiles = []
 
     def write_joblist(self, filename):
         with open(filename, mode='w') as f:
-            for i, file in enumerate(self.filenames,1):
+            for i, file in enumerate(self.jobFilenames,1):
                 f.write(f"{i},{file}\n")
     
     def include_additional_file(self, file, rename=''):
